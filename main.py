9c32d906eda0500dfeaf87d4e94d2c281dc122b0
ENUVE <sumansh.verma@claritusconsulting.com>
2024-02-15 12:17:03 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import subprocess
+
+
+def networkinfo():
+    subprocess.call(["ifconfig"])
+
+def diskusage():
+    subprocess.call(["df"])
+
+def makedir():
+    subprocess.call(["mkdir","testsumansh"])
+
+def removedir():
+    subprocess.call(["rm","testsumansh"])
+
+def ramusage():
+    subprocess.call(["top"])
+
+def adduser():
+    subprocess.call("")
+
+
+def main():
+    while True:
+        key = input("Press a number key (1-8): ")
+        if key == '1':
+            networkinfo()
+        
+        elif key == '2':
+            diskusage()
+
+        elif key =='3':
+             makedir()
+        
+        elif key =='4':
+            removedir()     
+
+        elif key =='5':
+            ramusage()
+
+        elif key =='6':
+            adduser()    
+
+        elif key == 'e':  # If you want to quit on 'q'
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid key. Please press a number key from 1-8.")
+
+if __name__ == "__main__":
+    main()
